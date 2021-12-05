e58b6037b90047a172b2128ae0b3c3e6413f21da
Matias Jacinto <matymdq2015@gmail.com>
2021-12-05 20:49:20 -0300

    funcionamiento login sesiones

--- app.js
+++ app.js
@@ -1,60 +1,82 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-require('dotenv').config(); //para el envio de formulario, carga los datos del .env , se coloca antes de que declare las paginas
+require("dotenv").config(); //para el envio de formulario, carga los datos del .env , se coloca antes de que declare las paginas
+var session = require("express-session");
 
-var indexRouter = require('./routes/index');
-var nosotrosRouter=require('./routes/nosotros');//nosotros.js, manejador, controlador
-var serviciosRouter=require('./routes/servicios');
-var galeriaRouter=require('./routes/galeria');
-var novedadesRouter=require('./routes/novedades');
-var contactoRouter=require('./routes/contacto');
-var loginRouter=require('./routes/admin/login');//admin login.js creamos app abajo
-
-
+var indexRouter = require("./routes/index");
+var nosotrosRouter = require("./routes/nosotros"); //nosotros.js, manejador, controlador
+var serviciosRouter = require("./routes/servicios");
+var galeriaRouter = require("./routes/galeria");
+var novedadesRouter = require("./routes/novedades");
+var contactoRouter = require("./routes/contacto");
+var loginRouter = require("./routes/admin/login"); //admin login.js creamos app abajo
+var adminNovedadesRouter = require("./routes/admin/novedades"); //admin novedades.js creamos app abajo
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 /*app.get('/', function(req,res,next){
   res.send('hola rey')
 })*/
 
-app.use('/', indexRouter);
-app.use('/nosotros', nosotrosRouter);
-app.use('/servicios', serviciosRouter);
-app.use('/galeria', galeriaRouter);
-app.use('/novedades', novedadesRouter);
-app.use('/contacto', contactoRouter);
-app.use('/admin/login', loginRouter);
+app.use(
+  session({
+    secret: "holaholaholaholaholahola",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
 
+secured = async function (req, res, next) {
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next();
+    } else {
+      res.redirect("/admin/login");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.use("/", indexRouter);
+app.use("/nosotros", nosotrosRouter);
+app.use("/servicios", serviciosRouter);
+app.use("/galeria", galeriaRouter);
+app.use("/novedades", novedadesRouter);
+app.use("/contacto", contactoRouter);
+app.use("/admin/login", loginRouter);
+app.use("/admin/novedades", secured, adminNovedadesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,5 +1,8 @@
 var express=require('express');
 var router=express.Router();
+var usuariosModel=require('./../../models/usuariosModels');//
+
+
 
 //recibir y mostrar ls vista de admin/login
 router.get('/', function(req,res,next){
@@ -8,6 +11,35 @@ router.get('/', function(req,res,next){
     })
 });
 
+router.post('/', async function(req,res,next){
+    try{
+        var usuario=req.body.usuario;
+        //este dato de la variable va a data
+        var password=req.body.password;
+        //este dato de la variable va a data
+
+
+        var data= await usuariosModel.getUserAndPassword(usuario, password);
+
+        if(data !=undefined){
+            req.session.id_usuario=data.id;
+            req.session.nombre=data.usuario;
+            
+            res.redirect('/admin/novedades');
+        }else{
+            res.render('admin/login',{
+                layout:'admin/layout',
+                error:true//si exite error de autentificacion coloco el error  en login.hbs para que se vea en pantalla, debajo del form
+
+            })
+        }
+
+    }catch(error){
+    console.log(error)
+    }
+
+
+})
 
 
 
