3d6b008be39cfd180c8a993b68f2e2f4d3b1fe81
Matias Jacinto <matymdq2015@gmail.com>
2021-12-06 01:06:53 -0300

    diseño admi+ listado novedades

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,10 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
+var novedadesModel = require("../../models/novedadesModels");
+
+router.get("/", async function (req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
+
   res.render("admin/novedades", {
     layout: "admin/layout",
     usuario: req.session.nombre,
+    novedades,
   });
 });
 
