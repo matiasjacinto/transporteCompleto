33e0d2f069fdab364b5835d30774207ae8f9d84a
Matias Jacinto <matymdq2015@gmail.com>
2021-12-07 22:54:33 -0300

    agregar -novedades

--- models/novedadesModels.js
+++ models/novedadesModels.js
@@ -5,5 +5,14 @@ async function getNovedades() {
   var rows = await pool.query(query);
   return rows;
 }
-
-module.exports = { getNovedades };
+async function insertNovedad(obj) {
+  try {
+    var query = "insert into novedades set ?";
+    var rows = await pool.query(query, [obj]);
+    return rows;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+module.exports = { getNovedades, insertNovedad };
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var novedadesModel = require("../../models/novedadesModels");
 
+//listado novedades
 router.get("/", async function (req, res, next) {
   var novedades = await novedadesModel.getNovedades();
 
@@ -11,6 +12,41 @@ router.get("/", async function (req, res, next) {
     usuario: req.session.nombre,
     novedades,
   });
+});
+
+//vista del formulario de agrgar
+router.get("/agregar", function (req, res, next) {
+  res.render("admin/agregar", {
+    layout: "admin/layout",
+  });
+});
+
+//procesa funcionamiento del boton agregar
+router.post("/agregar", async function (req, res, next) {
+  try {
+    console.log(req.body);
+    if (
+      req.body.titulo != "" &&
+      req.body.subtitulo != "" &&
+      req.body.contenido != ""
+    ) {
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect("/admin/novedades");
+    } else {
+      res.render("admin/agregar", {
+        layout: "admin/layout",
+        error: true,
+        message: "complete toods los campos",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.render("admin/agregar", {
+      layout: "admin/layout",
+      error: true,
+      message: "no se cargo la novedad",
+    });
+  }
 });
 
 module.exports = router;
