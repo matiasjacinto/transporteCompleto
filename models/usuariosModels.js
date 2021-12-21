09a4fa7c88cb933dbfa4f952288e831002f6eca9
Matias Jacinto <matymdq2015@gmail.com>
2021-12-20 23:43:52 -0300

    base de dato admin

--- models/usuariosModels.js
+++ models/usuariosModels.js
@@ -1,17 +1,16 @@
-var pool=require('./bd');
-var md5=require('md5');
+var pool = require("./bd");
+var md5 = require("md5");
 
-async function getUserAndPassword(user,password){
-    try{
-        var query='select * from usuarios where usuario=? and password=? limit 1';
-        var rows= await pool.query( query,[user,md5(password)]);
-        return rows[0]
-
-    }catch(error){
-        console.log(error)
-    }
+async function getUserAndPassword(user, password) {
+  try {
+    var query = "select * from usuarios where usuario=? and password=? limit 1";
+    var rows = await pool.query(query, [user, md5(password)]);
+    return rows[0];
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 //try catch estructuras de control para elmanejo de errores
 
-module.exports={getUserAndPassword}
+module.exports = { getUserAndPassword };
